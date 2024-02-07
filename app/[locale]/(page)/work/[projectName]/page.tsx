5db2b25a807978ab9f073dedc9bf9a48de50168d
dobzacode <corentin.kittel@gmail.com>
2024-02-07 19:35:02 +0100

    fix done

--- app/[locale]/(page)/work/[projectName]/page.tsx
+++ app/[locale]/(page)/work/[projectName]/page.tsx
@@ -12,12 +12,12 @@ export async function generateMetadata({
     title: t('title'),
     description: t('description'),
     openGraph: {
-      images: `url/public/og-image/${projectName}.jpg`,
+      images: `url/${projectName}.jpg`,
       title: t('title'),
       description: t('description')
     },
     twitter: {
-      images: `url/public/og-image/${projectName}.jpg`,
+      images: `url/${projectName}.jpg`,
       title: t('title'),
       description: t('description')
     }
