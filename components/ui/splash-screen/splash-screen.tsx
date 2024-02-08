3a488f3eb39f9223c6b40725ff076cbc6737b9f6
dobzacode <corentin.kittel@gmail.com>
2024-02-08 16:02:55 +0100

    fix splash screen

--- components/ui/splash-screen/splash-screen.tsx
+++ components/ui/splash-screen/splash-screen.tsx
@@ -26,20 +26,23 @@ export default function SplashScreen({}) {
   if (!visible || sessionStorage.getItem('shown')) return;
 
   return (
-    <div className="fixed inset-0 z-[80] flex h-full w-full items-center justify-center bg-primary1 dark:bg-primary99">
+    <div className="fixed inset-0 z-[80] flex h-screen w-screen items-center justify-center overflow-hidden bg-primary1 dark:bg-primary99">
       <div className=" slideInToRight flex h-[400px] w-[400px] flex-col items-center justify-center gap-small text-primary90 dark:text-primary1 ">
         {!isMobile && (
           <>
-            <MovingLogo className="-mb-extra-large -ml-sub-large -mt-extra-large dark:hidden max-mobile-large:hidden"></MovingLogo>
+            <MovingLogo className="-mb-[16rem] -ml-sub-large -mt-[16rem] dark:hidden max-mobile-large:hidden"></MovingLogo>
             <MovingLogo
               isDark={true}
-              className="-mb-extra-large -ml-sub-large -mt-extra-large hidden dark:block max-mobile-large:hidden"
+              className="-mb-[16rem] -ml-sub-large -mt-[16rem] hidden dark:block max-mobile-large:hidden"
             ></MovingLogo>
           </>
         )}
         {isMobile && <StaticLogo className="hidden w-40 max-mobile-large:block"></StaticLogo>}
         <div className="slideInFromLeft  flex items-end gap-small">
-          <P textType="heading" className="max-tablet:text-sub-heading">
+          <P
+            textType="heading"
+            className="max-tablet:text-sub-heading max-tablet:leading-sub-heading"
+          >
             {t('phrase')}{' '}
           </P>
           <PulseLoader
@@ -52,6 +55,16 @@ export default function SplashScreen({}) {
             className="relative z-20 -ml-[4rem] block dark:hidden max-tablet:scale-75"
             size={4}
           />
+        </div>
+      </div>
+      <div className=" absolute bottom-small left-1/2 m-auto -translate-x-1/2 transform text-primary90  dark:text-primary1  ">
+        <div className="slideInFromRight">
+          <P
+            textType={'sub-heading'}
+            className="slideInToRight max-tablet:text-body max-tablet:leading-body"
+          >
+            &#169; Corentin Kittel 2024
+          </P>
         </div>
       </div>
     </div>
