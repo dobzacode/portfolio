262cb040ad12173bb2f31f17c474bf17b576fe69
dobzacode <corentin.kittel@gmail.com>
2024-02-08 18:03:43 +0100

    fix splashscreen

--- components/ui/splash-screen/splash-screen.tsx
+++ components/ui/splash-screen/splash-screen.tsx
@@ -24,30 +24,27 @@ export default function SplashScreen({}) {
 
   return (
     <div className="fixed inset-0 z-[80] flex h-screen w-screen items-center justify-center overflow-hidden bg-primary1 dark:bg-primary99">
-      <div className="slideInToRight  flex h-[400px] w-[400px] flex-col items-center justify-center gap-small text-primary90 dark:text-primary1 ">
-        <div className="slideInFromLeft  flex  flex-col items-center justify-center gap-extra-small">
-          <P
-            textType="heading"
-            className="animate-pulse font-['HFF_Ultrasound'] max-tablet:text-sub-heading max-tablet:leading-sub-heading"
-          >
+      <div className="slideInToRight flex w-fit  flex-col items-center justify-center gap-small text-primary90 dark:text-primary1 ">
+        <div className="slideInFromLeft flex w-fit  flex-col items-center justify-center ">
+          <P textType="heading" className="animate-pulse font-['HFF_Ultrasound'] ">
             {t('phrase')}{' '}
           </P>
           <BarLoader
             color="#ffffff"
-            className="relative z-20 hidden  animate-pulse rounded-sm dark:block"
+            className="relative z-20 hidden animate-pulse rounded-sm dark:block"
           />
           <BarLoader
             color="#000000"
-            className="relative z-20 block  animate-pulse rounded-sm dark:hidden"
+            className="relative z-20 block animate-pulse rounded-sm dark:hidden"
           />
         </div>
       </div>
       <div className="absolute bottom-small left-1/2 z-50 m-auto -translate-x-1/2 transform  text-primary90   dark:text-primary1">
-        <div className="slideInToRight  z-50">
+        <div className="slideInToRight   z-50">
           <div className="slideInFromLeft z-50">
             <Link
               href="/legal"
-              className=" sub-heading leading-heading max-tablet:text-body max-tablet:leading-body"
+              className=" body leading-heading max-tablet:text-caption max-tablet:leading-caption"
             >
               &#169; Corentin Kittel 2024
             </Link>
